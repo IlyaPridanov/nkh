c73d4463c06238c8239143fafda12202115a3b2e
Ilya Pridanov <pridanov.ilya@mail.ru>
2020-07-17 16:11:27 +0400

    делает сборку

---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+// Подключение js-файлов с помощью rigger-а
+
+// Vendor
+
+//= vendor/details-element-polyfill.js
+//= vendor/imask.js
+
+// Modules
+//= modules/openMobile.js
+//= modules/openInput.js
+//= modules/openPopup.js
+//= modules/ajaxForm.js
+//= modules/formSend.js
+//= modules/formMask.js
+
